17fe2b45aba8ef8aadabfb67173634ddc6d05c33
tahaOsama <taha.alphasquad@gmail.com>
2022-06-09 18:47:55 +0500

    Basic Next App

---
+++ components/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+// make another folder for pricing in the components folder
+// make a new page in the pages folder
+// import pricing from "../components/pricing";
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import Home from "../components/home";
+
+export default function index() {
+  return <Home />;
+}
+
+// install node modules to run the app "npm install"
+// run the app "npm start"
+
+// Read components/home/index.js page for more info
